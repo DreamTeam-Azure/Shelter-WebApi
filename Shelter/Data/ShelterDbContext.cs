d9285f12b8da1d87b619d6abd3382635a178f73e
NataPavel <pashanatanov@gmail.com>
2022-01-13 19:09:56 +0200

    Добавлены Модели

---
+++ Shelter/Data/ShelterDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Shelter.Models;
+
+namespace Shelter.Data
+{
+    public class ShelterDbContext : DbContext
+    {
+        public ShelterDbContext() { }
+        public ShelterDbContext(DbContextOptions<ShelterDbContext> options)
+            : base(options)
+        { }
+        public DbSet<Kind> Kinds { get; set; }
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
---
+++ Shelter/Models/Kind.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shelter.Models
+{
+    public class Kind
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string Name { get; set; }
+
+        public virtual List<Animal> Animals { get; set; }
+    }
+}
